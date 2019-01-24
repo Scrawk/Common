477c76e19149da761ac528e6453e8dcdb6aed072
Justin Hawkins <29545213+Scrawk@users.noreply.github.com>
2019-01-24 20:13:49 +0800

    Improved Tests

--- Common.Meshing.Test/Constructors/CreateGeneralMesh2Test.cs
+++ Common.Meshing.Test/Constructors/CreateGeneralMesh2Test.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Common.Core.LinearAlgebra;
 using Common.Meshing.HalfEdgeBased;
 using Common.Meshing.Constructors;
+using Common.Meshing.Test.HalfEdgeBased;
 
 namespace Common.Meshing.Test.Constructors
 {
@@ -13,7 +14,7 @@ namespace Common.Meshing.Test.Constructors
         [TestMethod]
         public void FromTriangle()
         {
-            var constructor = new HBMeshConstructor<HBVertex2f, HBEdge, HBFace>();
+            var constructor = new HBMeshConstructor2f();
 
             var a = new Vector2f(-1, -1);
             var b = new Vector2f(1, -1);
@@ -29,19 +30,30 @@ namespace Common.Meshing.Test.Constructors
             Assert.AreEqual(a, mesh.Vertices[0].Position);
             Assert.AreEqual(b, mesh.Vertices[1].Position);
             Assert.AreEqual(c, mesh.Vertices[2].Position);
+
+            HBMeshHelper.CheckVertex(mesh, vertex: 0, edge: 0);
+            HBMeshHelper.CheckVertex(mesh, vertex: 1, edge: 1);
+            HBMeshHelper.CheckVertex(mesh, vertex: 2, edge: 2);
+            HBMeshHelper.CheckEdge(mesh, edge: 0, from: 0, face: 0, previous: 2, next: 1, opposite: 3);
+            HBMeshHelper.CheckEdge(mesh, edge: 1, from: 1, face: 0, previous: 0, next: 2, opposite: 4);
+            HBMeshHelper.CheckEdge(mesh, edge: 2, from: 2, face: 0, previous: 1, next: 0, opposite: 5);
+            HBMeshHelper.CheckEdge(mesh, edge: 3, from: 1, face: -1, previous: 4, next: 5, opposite: 0);
+            HBMeshHelper.CheckEdge(mesh, edge: 4, from: 2, face: -1, previous: 5, next: 3, opposite: 1);
+            HBMeshHelper.CheckEdge(mesh, edge: 5, from: 0, face: -1, previous: 3, next: 4, opposite: 2);
+            HBMeshHelper.CheckFace(mesh, face: 0, edge: 0);
+
+            HBMeshHelper.CheckAllTrianglesCCW(mesh);
         }
 
         [TestMethod]
         public void FromBox()
         {
-            var constructor = new HBMeshConstructor<HBVertex2f, HBEdge, HBFace>();
+            var constructor = new HBMeshConstructor2f();
 
             var min = new Vector2f(-1, -1);
             var max = new Vector2f(1, 1);
 
             CreateGeneralMesh2.FromBox(constructor, min, max);
-
-            constructor.AddBoundary = true;
             var mesh = constructor.PopMesh();
 
             Assert.AreEqual(4, mesh.Vertices.Count);
@@ -51,16 +63,28 @@ namespace Common.Meshing.Test.Constructors
             Assert.AreEqual(new Vector2f(max.x, min.y), mesh.Vertices[1].Position);
             Assert.AreEqual(max, mesh.Vertices[2].Position);
             Assert.AreEqual(new Vector2f(min.x, max.y), mesh.Vertices[3].Position);
+
+            HBMeshHelper.CheckVertex(mesh, vertex: 0, edge: 0);
+            HBMeshHelper.CheckVertex(mesh, vertex: 1, edge: 1);
+            HBMeshHelper.CheckVertex(mesh, vertex: 2, edge: 2);
+            HBMeshHelper.CheckVertex(mesh, vertex: 3, edge: 3);
+            HBMeshHelper.CheckEdge(mesh, edge: 0, from: 0, face: 0, previous: 3, next: 1, opposite: 4);
+            HBMeshHelper.CheckEdge(mesh, edge: 1, from: 1, face: 0, previous: 0, next: 2, opposite: 5);
+            HBMeshHelper.CheckEdge(mesh, edge: 2, from: 2, face: 0, previous: 1, next: 3, opposite: 6);
+            HBMeshHelper.CheckEdge(mesh, edge: 3, from: 3, face: 0, previous: 2, next: 0, opposite: 7);
+            HBMeshHelper.CheckEdge(mesh, edge: 4, from: 1, face: -1, previous: 5, next: 7, opposite: 0);
+            HBMeshHelper.CheckEdge(mesh, edge: 5, from: 2, face: -1, previous: 6, next: 4, opposite: 1);
+            HBMeshHelper.CheckEdge(mesh, edge: 6, from: 3, face: -1, previous: 7, next: 5, opposite: 2);
+            HBMeshHelper.CheckEdge(mesh, edge: 7, from: 0, face: -1, previous: 4, next: 6, opposite: 3);
+            HBMeshHelper.CheckFace(mesh, face: 0, edge: 0);
         }
 
         [TestMethod]
         public void FromCircle()
         {
-            var constructor = new HBMeshConstructor<HBVertex2f, HBEdge, HBFace>();
+            var constructor = new HBMeshConstructor2f();
 
             CreateGeneralMesh2.FromCircle(constructor, Vector2f.Zero, 1.0f, 4);
-
-            constructor.AddBoundary = true;
             var mesh = constructor.PopMesh();
 
             Assert.AreEqual(4, mesh.Vertices.Count);
