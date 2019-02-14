98d0014eee8254ef991cf12832ff79cd5a5cc017
Justin Hawkins <scrawkdog@hotmail.com>
2019-02-14 16:44:31 +0800

    Changes to shuffle

--- Common.Core/Extensions/IListExtension.cs
+++ Common.Core/Extensions/IListExtension.cs
@@ -8,11 +8,22 @@ namespace System.Collections.Generic
     public static class IListExtension
     {
 
-        public static void Shuffle<T>(this IList<T> list, Random rnd = null)
+        public static void Shuffle<T>(this IList<T> list, int seed)
         {
-            if (rnd == null)
-                rnd = new Random();
+            var rnd = new Random(seed);
 
+            int n = list.Count;
+            while (n > 1)
+            {
+                int k = rnd.Next(n--);
+                T temp = list[n];
+                list[n] = list[k];
+                list[k] = temp;
+            }
+        }
+
+        public static void Shuffle<T>(this IList<T> list, Random rnd)
+        {
             int n = list.Count;
             while (n > 1)
             {
