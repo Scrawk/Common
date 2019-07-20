2fbfc95ff958228209c6e95fbefe0f64749bf611
Justin Hawkins <scrawkdog@hotmail.com>
2019-07-20 18:59:26 +0800

    Added bit

---
+++ Common.Core.Test/Bits/BitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Common.Core.Bits;
+
+namespace Common.Core.Test.Bits
+{
+    [TestClass]
+    public class BitTest
+    {
+        [TestMethod]
+        public void SetBit()
+        {
+
+        }
+    }
+}
---
+++ Common.Core/Bits/Bit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.Core.Bits
+{
+    public static class Bit
+    {
+
+        public static void Set(ref int value, int position)
+        {
+            value |= (1 << position);
+        }
+
+        public static bool IsSet(int value, int position)
+        {
+            return (value & (1 << position)) != 0;
+        }
+
+        public static void Clear(ref int value, int position)
+        {
+            value &= ~(1 << position);
+        }
+
+        public static void Flip(ref int value, int position)
+        {
+            value ^= (1 << position);
+        }
+    }
+}
