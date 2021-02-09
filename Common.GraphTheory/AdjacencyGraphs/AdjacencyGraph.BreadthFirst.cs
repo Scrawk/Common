4af156cc494f3630850e0c8f1caa6ecf1523cc65
Justin Hawkins <scrawkdog@hotmail.com>
2021-02-09 21:24:00 +0800

    added breadth first with max dist

--- Common.GraphTheory/AdjacencyGraphs/AdjacencyGraph.BreadthFirst.cs
+++ Common.GraphTheory/AdjacencyGraphs/AdjacencyGraph.BreadthFirst.cs
@@ -35,9 +35,57 @@ namespace Common.GraphTheory.AdjacencyGraphs
                 for (int i = 0; i < edges.Count; i++)
                 {
                     int to = edges[i].To;
-
                     if (Vertices[to].Tag == IS_VISITED_TAG) continue;
 
+                    queue.Enqueue(to);
+                    Vertices[to].Tag = IS_VISITED_TAG;
+                }
+            }
+
+            return ordering;
+        }
+
+        /// <summary>
+        /// Return a list of vertex indices ordered breadth first
+        /// but stopping when max distance is reached.
+        /// </summary>
+        /// <param name="root">The vertex index to start at.</param>
+        /// <param name="DistanceFunc">The function used to calculate the distance between two vertices.</param>
+        /// <param name="maxDistance">The distance to stop at.</param>
+        /// <returns>The vertices in breadth first order.</returns>
+        public List<int> BreadthFirstOrder(int root, Func<GraphVertex, GraphVertex, float> DistanceFunc, float maxDistance)
+        {
+            TagVertices(NOT_VISITED_TAG);
+            int count = VertexCount;
+
+            var queue = new Queue<int>(count);
+            queue.Enqueue(root);
+
+            Vertices[root].Tag = IS_VISITED_TAG;
+            Vertices[root].Cost = 0;
+
+            var ordering = new List<int>(count);
+
+            while (queue.Count != 0)
+            {
+                int u = queue.Dequeue();
+                ordering.Add(u);
+
+                var edges = Edges[u];
+                if (edges == null) continue;
+
+                var fromVert = Vertices[u];
+
+                for (int i = 0; i < edges.Count; i++)
+                {
+                    int to = edges[i].To;
+                    if (Vertices[to].Tag == IS_VISITED_TAG) continue;
+
+                    var toVert = Vertices[to];
+                    float dist = fromVert.Cost + DistanceFunc(fromVert, toVert);
+                    if (dist > maxDistance) continue;
+
+                    toVert.Cost = dist;
                     queue.Enqueue(to);
                     Vertices[to].Tag = IS_VISITED_TAG;
                 }
