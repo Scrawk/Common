2f6350b2aa4e2f8ad077b9a37f05cf81a957e376
Justin Hawkins <scrawkdog@hotmail.com>
2022-06-11 14:20:13 +0800

    added binaries

--- Common.Console/Program.cs
+++ Common.Console/Program.cs
@@ -17,33 +17,6 @@ namespace Common.Console
 
         static void Main(string[] args)
         {
-            var graph = new FlowGridGraph(128, 128, true);
-       
-            var rnd = new Random(0);
-
-            graph.Iterate((x, y) =>
-            {
-                int capacity = rnd.Next(1, 255);
-                graph.SetCapacity(x, y, capacity);
-            });
-
-            int width = 2;
-            graph.SetLabelAndCapacityInPerimeter(width, FLOW_GRAPH_LABEL.SOURCE, 255);
-
-            int offset = 16;
-            var bounds = new Box2i(offset, offset, graph.Width - 1 - offset, graph.Height - 1 - offset);
-            graph.SetLabelAndCapacityInBounds(bounds, FLOW_GRAPH_LABEL.SINK, 255);
-
-            var timer = new Timer();
-            timer.Start();
-
-            float maxflow = graph.Calculate();
-
-            timer.Stop();
-
-            WriteLine("Max flow " + maxflow);
-            WriteLine("Time " + timer.ElapsedMilliseconds);
-
         }
 
         static void WriteLine(object obj)
